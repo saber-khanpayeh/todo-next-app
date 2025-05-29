19e2d207867edbbe9131d2e1f074d80e01641629
Saber Khanpayeh <saber.dev.programming@gmail.com>
2025-05-30 01:27:08 +0330

    loading component added

---
+++ components/element/Loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Loading() {
+  return (
+    <div className="loading">
+      <span className="loading__circle"></span>
+    </div>
+  );
+}
+
+export default Loading;
--- components/template/HomePage.js
+++ components/template/HomePage.js
@@ -3,9 +3,11 @@ import Tasks from "../module/Tasks";
 import { TbNotesOff } from "react-icons/tb";
 import { MdFormatListBulletedAdd } from "react-icons/md";
 import EmptyTodo from "../element/EmptyTodo";
+import Loading from "../element/Loading";
 
 function HomePage() {
   const [todos, setTodos] = useState([]);
+  const [catchData,setCatchData]=useState("");
   useEffect(() => {
     fetchTodos();
   }, []);
@@ -13,20 +15,23 @@ function HomePage() {
     const res = await fetch("/api/todos");
     const data = await res.json();
     console.log(data);
+    setCatchData(data.status);
     if (data.status === "success") setTodos(data.data.todos);
   };
   return (
     <div className="home-page">
       <div className="home-page--todo">
         <p>Todo</p>
+        {!catchData &&<Loading/>}
         {todos.todo ? (
           <Tasks data={todos.todo} fetchTodos={fetchTodos} next="inProgress" />
         ) : (
-          <EmptyTodo todoType="todo" />
+          catchData?<EmptyTodo todoType="todo" />:null
         )}
       </div>
       <div className="home-page--inProgress">
         <p>In Progress</p>
+        {!catchData &&<Loading/>}
         {todos.inProgress ? (
           <Tasks
             data={todos.inProgress}
@@ -35,11 +40,12 @@ function HomePage() {
             back="todo"
           />
         ) : (
-          <EmptyTodo todoType="inProgress" />
+          catchData?<EmptyTodo todoType="inProgress" />:null
         )}
       </div>
       <div className="home-page--review">
         <p>Review</p>
+        {!catchData &&<Loading/>}
         {todos.review ? (
           <Tasks
             data={todos.review}
@@ -48,15 +54,16 @@ function HomePage() {
             back="inProgress"
           />
         ) : (
-          <EmptyTodo todoType="review" />
+          catchData?<EmptyTodo todoType="review" />:null
         )}
       </div>
       <div className="home-page--done">
         <p>Done</p>
+        {!catchData &&<Loading/>}
         {todos.done ? (
           <Tasks data={todos.done} fetchTodos={fetchTodos} back="review" />
         ) : (
-          <EmptyTodo todoType="done" />
+          catchData?<EmptyTodo todoType="done" />:null
         )}
       </div>
     </div>
