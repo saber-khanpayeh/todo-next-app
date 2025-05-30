27606c726c64b5000ad4dfe4de85e53744f40906
Saber Khanpayeh <saber.dev.programming@gmail.com>
2025-05-31 01:03:39 +0330

    pwa feature was  developed

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development", //disable  pwa in dev mode
+});
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig);
--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,17 @@ import { SessionProvider } from "next-auth/react";
 import Layout from "../components/layout/Layout";
 import "../styles/globals.css";
 import { ToastContainer } from "react-toastify";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <SessionProvider session={pageProps.session}>
       <Layout>
+        <Head>
+          <title>Planno App</title>
+          <link rel="icon" href="/favicon.png" />
+          <link rel="manifest" href="/manifest.json" />
+        </Head>
         <Component {...pageProps} />
         <ToastContainer/>
       </Layout>
